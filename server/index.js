e4b50ed22eb20fac53f6f3ea3fd65a44ea434a7a
Adrian Meza <adrianme213@gmail.com>
2017-11-03 14:34:19 -0700

    webpack.config.js file bundling and rendering to html index

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hi');
-})
-app.use(express.static(path.resolve(__dirname, '../client')));
-
 const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+// app.get('/', function(req, res) {
+//   res.send('Hi');
+// })
+
 app.listen(port, function() {
   console.log('Listening on: ', port)
 });
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'es2015', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
